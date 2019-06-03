db166389480262387e5c4a53df23774a7c141175
Josh Dzielak <jdzielak@gmail.com>
2019-06-03 19:23:51 +0200

    Link to source code in footer

--- components/footer.js
+++ components/footer.js
@@ -22,6 +22,7 @@ const Footer = ({ index, theme }) => (
   >
     <li>{index}</li>
     <li><a href={`https://twitter.com/hashtag/${theme.metadata.hashtag}`}>{theme.metadata.hashtag}</a></li>
+    <li><a href={theme.metadata.repository} style={{ textDecoration: `underline` }}>get the code</a></li>
     <li css={{ flexGrow: 1 }}></li>
     <li>{theme.metadata.author}</li>
     <li><a href={`https://twitter.com/${theme.metadata.twitter}`}>{theme.metadata.twitter}</a></li>
